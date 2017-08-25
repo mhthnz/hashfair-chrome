f280de339141d378d182f9bfa73490764b8eb702
Francesco <f.strazzante@gmail.com>
2017-08-25 11:24:51 +0200

    Create ita.js

---
+++ languages/ita.js
@@ -0,0 +1,47 @@
+{
+    UI: {
+        Close: 'Chiudi',
+        AboutMe: 'Chi sono',
+        LastSummaryBtc: 'L'ultimo pagamento (SHA, SCRYPT)',
+        DailyAverage: 'Media giornaliera sul periodo di {days} giorni (SHA, SCRYPT)'
+    },
+    donate: {
+        title: 'Chi sono :)',
+        start_text: 'Sono lo sviluppatore di questo plugin. Lavoro sul plugin volontariamente. Sono motivato dalle vostre donazioni e grazie.',
+        help_me: 'Puoi ringraziarmi in diversi modi',
+        github: 'Puoi votare il plugin o eseguire un fork a questo indirizzo {url}',
+        chrome_market: 'Puoi votare in {url}',
+        btc: 'Puoi inviare qualche bitcoin al mio portafoglio: <b>19VWsFYgh9eB6j42A1kg6yyWmRePyQQrRv</b>',
+        telegram: 'Puoi ringraziarmi personalmente in telegram: <b><a target="_blank" href="https://t.me/mhthnz">@mhthnz</a></b>',
+        thank: 'Grazie a tutti quelli che contribuiranno <b style="color:red;"><3</b>'
+    },
+    settings: {
+        settings: 'Impostazioni del plugin',
+        languageSetting: 'Impostazioni della lingua',
+        autodetectLanguage: 'Scegli la lingua automaticamente',
+        eng: 'Inglese',
+        rus: 'Russo',
+        modulesSetting: 'Modules for initiate',
+        revenueShaForecast: 'Number of days to calculate SHA revenue forecast',
+        revenueScryptForecast: 'Number of days to calculate SCRYPT revenue forecast',
+        days: '{days} d.',
+        save: 'Save',
+        settingsSaved: 'Settings saved'
+    },
+    modules: {
+        Balance: 'Show balance and bitcoin price.',
+        BalanceChart: 'Show clear balance chart.',
+        DashBalance: 'Show dash balance in usd.',
+        EthBalance: 'Show eth balance in usd.',
+        LastAvgBtc: 'Show daily average.',
+        LastScryptPayout: 'Show clear last scrypt payout.',
+        LastShaPayout: 'Show clear last sha payout.',
+        LastSummaryBtcPayout: 'Show last summary clear payout (SHA + SCRYPT).',
+        ScryptChart: 'Show clear SCRYPT chart revenue per 1 MH/s.',
+        ScryptForecast: 'Show clear SCRYPT revenue forecast.',
+        ScryptHashrate: 'Show clear SCRYPT hashrate for now.',
+        ShaChart: 'Show clear SHA chart revenue per 1 TH/s.',
+        ShaForecast: 'Show clear SHA revenue forecast.',
+        ShaHashrate: 'Show clear SHA hashrate for now.'
+    }
+}
