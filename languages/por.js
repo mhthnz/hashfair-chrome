e2156530c3561c637a80cd84cdc881169e4a55ec
Marcelo Barbosa <firemanxbr@fedoraproject.org>
2017-08-27 23:33:01 +0700

    added portuguese(por) language

---
+++ languages/por.js
@@ -0,0 +1,65 @@
+{
+    UI: {
+        Close: 'Fechar',
+        AboutMe: 'Sobre mim',
+        LastSummaryBtc: 'Último resumo do pagamento em btc (SHA, SCRYPT)',
+        DailyAverage: 'Média diária em {days} dias do período (SHA, SCRYPT)'
+    },
+    charts: {
+        Payout: 'Pagamento',
+        maintenance: 'Manutenção'
+    },
+    pools: {
+        pool: 'Pool',
+        hashrate: 'Hashrate',
+        lucky: 'Sorte',
+        your_share: 'Sua parte',
+        distribution: 'Distribuição',
+        sha_header: 'Sha Pools Hashrate',
+        scrypt_header: 'Scrypt Pools Hashrate',
+        blocks_per_hour: 'Blocos por hora'
+    },
+    donate: {
+        title: 'Sobre mim :)',
+        start_text: 'Eu sou o desenvolvedor deste plugin. Estou trabalhando sobre este plugin de forma voluntária. Estou motivado por suas doações e obrigado.',
+        help_me: 'Você pode me agradecer de várias maneiras',
+        github: 'Você pode conceder-me uma estrela e criar um forke em {url}',
+        chrome_market: 'Você pode conceder-me uma estrela em {url}',
+        btc: 'Você pode enviar-me alguns bitcoins para minha carteira: <b>19VWsFYgh9eB6j42A1kg6yyWmRePyQQrRv</b>',
+        telegram: 'Você pode agradecer-me pessoalmente no telegram: <b><a target="_blank" href="https://t.me/mhthnz">@mhthnz</a></b>',
+        thank: 'Obrigado a todos que responderam <b style="color:red;"><3</b>'
+    },
+    settings: {
+        settings: 'Configurações do plugin',
+        languageSetting: 'Configuração de idioma',
+        autodetectLanguage: 'Detectar idioma automaticamente',
+        eng: 'English',
+        rus: 'Russian',
+        ita: 'Italiano',
+        por: 'Portuguese',
+        modulesSetting: 'Módulos para iniciar',
+        revenueShaForecast: 'Número de dias para calcular a previsão de receita de SHA',
+        revenueScryptForecast: 'Número de dias para calcular a previsão de receita de SCRYPT',
+        days: '{days} d.',
+        save: 'Salvar',
+        settingsSaved: 'Configurações salvas'
+    },
+    modules: {
+        Balance: 'Mostrar o saldo e preço do bitcoin.',
+        BalanceChart: 'Mostrar o gráfico líquido no balanço.',
+        DashBalance: 'Mostrar o balanço de dash em usd.',
+        EthBalance: 'Mostrar o balanço de eth em usd.',
+        LastAvgBtc: 'Mostrar média diária.',
+        LastScryptPayout: 'Mostrar o último pagamento líquido de scrypt.',
+        LastShaPayout: 'Mostrar o último pagamento líquido de sha.',
+        LastSummaryBtcPayout: 'Mostrar último resumo líquido de pagamento (SHA + SCRYPT).',
+        ScryptChart: 'Mostrar a receita líquida do gráfico de SCRYPT por 1 MH/s.',
+        ScryptForecast: 'Mostrar a receita líquida prevista de SCRYPT.',
+        ScryptHashrate: 'Mostrar o hashrate líquido do SCRYPT atual.',
+        ShaChart: 'Mostrar a receita líquida do gráfico SHA por 1 TH/s.',
+        ShaForecast: 'Mostrar a receita líquida prevista de SHA.',
+        ShaHashrate: 'Mostrar hashrate líquido de SHA atual.',
+        ShaPools: 'Mostrar sha pools hashrate.',
+        ScryptPools: 'Mostrar scrypt pools hashrate.'
+    }
+}
