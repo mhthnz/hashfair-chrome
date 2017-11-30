1f5e500ed3f015c19e650511565b7e5a46e66a6f
Murat Gül <muratgul@gmail.com>
2017-11-30 18:40:13 +0300

    Create tur.js

---
+++ languages/tur.js
@@ -0,0 +1,85 @@
+{
+    UI: {
+        Close: 'Kapat',
+        AboutMe: 'Hakkımda',
+        LastSummaryBtc: 'Son BTC ödeme özeti (SHA, SCRYPT)',
+        DailyAverage: 'Günlük ortalama {days} gün periyodu (SHA, SCRYPT)'
+    },
+    charts: {
+        payout: 'Ödeme',
+        maintenance: 'Bakım'
+    },
+    pools: {
+        pool: 'Havuz',
+        hashrate: 'Hashrate',
+        lucky: 'Şans',
+        your_share: 'Payın',
+        distribution: 'Dağıtım',
+        sha_header: 'Sha Havuz Hashrate',
+        scrypt_header: 'Scrypt Havuz Hashrate',
+        blocks_per_hour: 'Saatlik Blokl'
+    },
+    donate: {
+        title: 'Hakkımda :)',
+        start_text: 'Bu eklentinin geliştiricisiyim. Eklentiler üzerinde gönüllü çalışıyorum. Bağışlarınız ve teşekkürleriniz beni motive ediyor.',
+        help_me: 'Bana çeşitli şekillerde teşekkür edebilirsin.',
+        github: 'Bana yol verdin ve çatalladın {url}',
+        chrome_market: 'Bana yıldız verebilirsin {url}',
+        btc: 'Cüzdan üzerinden biraz coin gönderebilirsin: <b>19VWsFYgh9eB6j42A1kg6yyWmRePyQQrRv</b>',
+        telegram: 'Telegramla şahsen teşekkür edebilirsin: <b><a target="_blank" href="https://t.me/mhthnz">@mhthnz</a></b>',
+        thank: 'Yanıtlayanların hepsine teşekkürler. <b style="color:red;"><3</b>'
+    },
+    settings: {
+        settings: 'Eklenti ayarı',
+        languageSetting: 'Dil ayarı',
+        autodetectLanguage: 'Dili otomatik olarak algıla',
+        eng: 'English',
+        rus: 'Russian',
+        ita: 'Italiano',
+        por: 'Portuguese',
+        ind: 'Indonesia',
+        tur: 'Türkçe',
+        modulesSetting: 'Başlatma modülleri',
+        revenueShaForecast: 'SHA gelir tahmini hesaplamak için gün sayısı',
+        revenueScryptForecast: 'SYCRPT gelir tahmini hesaplamak için gün sayısı',
+        days: '{days} gün.',
+        save: 'Kaydet',
+        settingsSaved: 'Ayar kaydedildi'
+    },
+    modules: {
+        Balance: 'Bakiyeyi ve Bitcoin fiyatını göster.',
+        BalanceChart: 'Net bakiye grafiğini göster.',
+        DashBalance: 'Dash bakiyesini usd olarak göster.',
+        EthBalance: 'ETH bakiyesini usd olarak göster.',
+        LastAvgBtc: 'Günlük ortalamayı göster.',
+        LastScryptPayout: 'Son net scrypt ödemesini göster.',
+        LastShaPayout: 'Son net sha ödemesini göster.',
+        LastSummaryBtcPayout: 'Son net ödeme özetini göster (SHA + SCRYPT).',
+        ScryptChart: '1 MH/s başına net SCRYPT grafiğini göster.',
+        ScryptForecast: 'SCRYPT için net gelir tahminini göster.',
+        ScryptHashrate: 'Şimdiki net SCRYPT hashrate oranını göster.',
+        ShaChart: '1 TH/s başına net SHA grafiğini göster.',
+        ShaForecast: 'SHA için gelir tahminini göster.',
+        ShaHashrate: 'Şimdiki net SHA hashrate oranını göster.',
+        ShaPools: 'SHA havuz hashrate oranını göster.',
+        ScryptPools: 'Scrypt havuz hashrate oranını göster.',
+        PaybackBtc: "BTC Sözleşmelerinin geri ödemelerini hesapla."
+    },
+    payback: {
+        btc_payback: 'BTC Geri ödeme',
+        purchases: 'Alımlar',
+        number_of_purchases: 'Alım sayısı',
+        total_invested: 'Toplam yatırımlar',
+        invested_without_reinvest: 'Yatırımsız yatırım',
+        withdrawals: 'Çekme',
+        number_of_withdrawals: 'Çekme sayısı',
+        total_out_amount: 'Toplam tutar',
+        amount_with_comission: 'Komisyonlu tutar',
+        payback: 'Geri ödeme',
+        payback_percent: 'Geri ödeme yüzdesi',
+        payback_percent_with_comssion: 'Geri ödeme yüzdesi (komisyonlu)',
+        payback_without_reinvest: 'Geri ödeme (yatırımsız)',
+        payback_complete: 'Geri ödeme bitişi',
+        payback_complete_days: '{days} gün.',
+    }
+}
